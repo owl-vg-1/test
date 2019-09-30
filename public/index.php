9d8b0e972208521597a29f9fbcd5305fbe21d6e4
owl-vg-1 <47003077+owl-vg-1@users.noreply.github.com>
2019-10-01 00:54:14 +0300

    finish my calc

--- public/index.php
+++ public/index.php
@@ -1,21 +1,65 @@
 <?php
 
 use App\Calk;
-use App\Test;
 require '../vendor/autoload.php';
-
-
-echo 123;
-
-$obj = new Test;
-
-$calc = new Calk;
-
-$calc->divide(5, 6);
-
-$calc->multiply(6, 6);
-
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>Calculator</title>
+    <link rel="stylesheet" href="public/css/style.css">
+
+
+</head>
+<body>
+    <div class="calc">
+        <form method="post" action="#" autocomplete="off">
+            <div class="number">
+                <label class="number" for="numberA">Введите первое число:</label>
+                <input type="text" name="numberA" id="numberA">
+                <label class="number" for="numberB">Введите второе число (при необходимости):</label>
+                <input type="text" name="numberB" id="numberB">
+            </div>
+
+        <div class="row">
+            <label><input class="button-calc" type="submit" name="action" value="*"></label>
+            <label><input class="button-calc" type="submit" name="action" value="/"></label>
+            <label><input class="button-calc" type="submit" name="action" value="+"></label>
+        </div>
+
+
+        <div class="row">
+            <label><input class="button-calc" type="submit" name="action" value="-"></label>
+            <label><input class="button-calc" type="submit" name="action" value="log"></label>
+            <label><input class="button-calc" type="submit" name="action" value="pow"></label>
+        </div>
+        <div class="row">
+            <label><input class="button-calc" type="submit" name="action" value="&#8730"></label>
+            <label><input class="button-calc" type="submit" name="action" value="sin"></label>
+            <label><input class="button-calc" type="submit" name="action" value="cos"></label>
+        </div>
+        <div class="row">
+            <label><input class="button-calc" type="submit" name="action" value="hypot"></label>
+        </div>
+
+        </form>
+
+        <div class="result">
+            <p>Результат:
+                <?php
+                // print_r($_POST);
+                $obj = new Calk ($_POST);
+                echo $obj->calculate();   
+                ?>
+
+            </p>
+        </div>
+    </div>
+
+</body>
+</html>
--- src/Calk.php
+++ src/Calk.php
@@ -5,12 +5,92 @@ namespace App;
 class Calk implements CalcInterface
 {
     
+    function __construct() {
+        $this->numberA = $_POST['numberA'];
+        $this->numberB = $_POST['numberB'];
+        $this->action = $_POST['action'];
+    }
+
+
+
+    public function calculate()
+    {
+        switch ($this->action) {
+            case '*':
+                return $this->multiply($this->numberA, $this->numberB);
+                break;
+            case '/':
+                return $this->divide($this->numberA, $this->numberB);
+                break;
+            case '+':
+                return $this->plus($this->numberA, $this->numberB);
+                break;
+            case '-':
+                return $this->minus($this->numberA, $this->numberB);
+                break;
+            case 'log':
+                return $this->logarithm($this->numberA, $this->numberB);
+                break;
+            case 'pow':
+                return $this->pow($this->numberA, $this->numberB);
+                break;
+            case '√':
+                return $this->sqrt($this->numberA);
+                break;
+            case 'sin':
+                return $this->sin($this->numberA);
+                break;
+            case 'cos':
+                return $this->cos($this->numberA);
+                break;
+            case 'hypot':
+                return $this->hypot($this->numberA, $this->numberB);
+                break;
+            default:
+                # code...
+                break;
+        }
+    }
+
+
     public function multiply(float $a, float $b): float {
         return ($a*$b);
     }
     
     public function divide(float $a, float $b): float {
         return ($a/$b); 
+    }
+
+    public function plus(float $a, float $b): float {
+        return ($a+$b); 
+    }
+
+    public function minus(float $a, float $b): float {
+        return ($a-$b); 
+    }
+
+    public function logarithm(float $a, float $b): float {
+        return log($a, $b); 
+    }
+
+    public function pow(float $a, float $b): float {
+        return pow($a, $b); 
+    }
+    
+    public function sqrt(float $a): float {
+        return sqrt($a); 
+    }
+
+    public function sin(float $a): float {
+        return sin(deg2rad($a)); 
+    }
+
+    public function cos(float $a): float {
+        return cos(deg2rad($a)); 
+    }
+
+    public function hypot(float $a, float $b): float {
+        return hypot($a, $b); 
     }
 
 }
